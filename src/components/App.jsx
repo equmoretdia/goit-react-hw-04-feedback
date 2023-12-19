2a5c8440d3b6ae84fb56bb623ef6ffaeb5ec83a4
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-19 22:17:18 +0100

    hook refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,55 @@
-import React from 'react';
+import { useState } from 'react';
 import css from './App.module.css';
 import Section from './Section';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Notification from './Notification';
 
-class App extends React.Component {
-  state = {
+export default function App() {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  addFeadback = e => {
+  const addFeadback = e => {
     // console.log(e.target);
     const { name } = e.target;
-    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+    setState(prev => ({ ...prev, [name]: prev[name] + 1 }));
   };
 
-  countTotalFeedback = ({ good, neutral, bad } = this.state) =>
+  const countTotalFeedback = ({ good, neutral, bad } = state) =>
     good + neutral + bad;
 
-  countPositiveFeedbackPercentage = ({ good, neutral, bad } = this.state) => {
+  const countPositiveFeedbackPercentage = ({ good, neutral, bad } = state) => {
     const total = good + neutral + bad;
     const percentage = total > 0 ? (good / total) * 100 : 0;
     return percentage.toFixed(2) + '%';
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-    const total = this.countTotalFeedback();
-    const positive = this.countPositiveFeedbackPercentage();
-    return (
-      <div className={css.app}>
-        <h1 className={css.hidden}>Feedback Widget</h1>
-        <Section title="Please leave your feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.addFeadback}
+  const { good, neutral, bad } = state;
+  const options = Object.keys(state);
+  const total = countTotalFeedback();
+  const positive = countPositiveFeedbackPercentage();
+  return (
+    <div className={css.app}>
+      <h1 className={css.hidden}>Feedback Widget</h1>
+      <Section title="Please leave your feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={addFeadback} />
+      </Section>
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positive}
           />
-        </Section>
-        <Section title="Statistics">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positive}
-            />
-          ) : (
-            <Notification message="There is no feedback yet" />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        ) : (
+          <Notification message="There is no feedback yet" />
+        )}
+      </Section>
+    </div>
+  );
 }
-
-export default App;
